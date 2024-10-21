3212522a02f06697bccdcc364678375e93be3cce
AMD <90222287+michojekunle@users.noreply.github.com>
2024-10-21 03:01:36 +0100

    Update buyerController.ts

--- Backend/src/controllers/buyerController.ts
+++ Backend/src/controllers/buyerController.ts
@@ -98,3 +98,28 @@ export const loginBuyer = async (req: Request, res: Response) => {
         res.status(500).json({ message: 'Server error during buyer login' });
     }
 };
+
+export const getBuyerById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        const buyer = await Buyer.findById(id);
+        if (!buyer) {
+            return res.status(404).json({ message: 'Buyer not found' });
+        }
+        return res.status(200).json(buyer);
+    } catch (error) {
+        console.error('Error fetching this buyer by ID:', error);
+        res.status(500).json({ message: 'Server error fetching this buyer by ID' });
+    }
+};
+
+export const getAllBuyers = async (req: Request, res: Response) => {
+    try {
+        const buyers = await Buyer.find();
+        return res.status(200).json(buyers);
+    } catch (error) {
+        console.error('Error fetching all buyers:', error);
+        res.status(500).json({ message: 'Server error fetching all buyers' });
+    }
+};
